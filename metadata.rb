cfe911402fbc204ef27e280eeb3f68267627cd23
nmcginnis <nmcginnis@gmti.gannett.com>
2015-06-30 15:43:19 -0400

    GDPHAR-708 - initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'celery'
+maintainer       'Gannett'
+maintainer_email 'nmcginnis@gannett.com'
+license          'All rights reserved'
+description      'Installs/Configures chef-celery'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+recipe 'celery', 'Installs the Celery pip module'
+
+depends 'python'
